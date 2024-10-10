232b513b150f2802176ed236b14611d4c3f80fb8
Luke McCartin <ldmcartin@gmail.com>
2024-10-10 13:03:45 +0200

    add config and stuff

---
+++ src/config.rs
@@ -0,0 +1,28 @@
+use serde_derive::{Serialize, Deserialize};
+
+static PACKAGE_NAME: &str = env!("CARGO_PKG_NAME");
+
+pub fn set_config(branch_prefix: String) {
+    let new_config = LazyGitConfig {
+        branch_prefix: branch_prefix.clone()
+    };
+
+    match confy::store(PACKAGE_NAME, None, new_config) {
+        Ok(_val) => println!("Branch prefix set to: {:?}", branch_prefix),
+        Err(err) => println!("{:?}", err)
+    }
+}
+
+pub fn display_conifg() {
+    let config: LazyGitConfig = confy::load(PACKAGE_NAME, None).unwrap();
+    println!("{:?}", config);
+}
+
+pub fn get_config() -> LazyGitConfig {
+    return confy::load(PACKAGE_NAME, None).unwrap();
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct LazyGitConfig {
+    pub branch_prefix: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use clap::{Parser, Subcommand};
-use std::{fmt::format, process::Command};
+use regex::Regex;
+use std::process::Command;
+use lib::config;
+use lib::commands;
 
 #[derive(Parser)]
-#[command(name = "git-helper", version = "1.0", about = "A CLI tool for managing branches and commits")]
+#[command(name = "lazy-git", version = "1.0", about = "A CLI tool for managing branches and commits")]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -16,18 +19,30 @@ enum Commands {
     /// Commits with a message
     C(CommitArgs),
 
-    /// Adds all changes with no additional options
-    AA,
-    RA,
+    /// Stages all changes
+    A,
+
+    /// Resets all changes
+    R,
+
+    /// Set Config for CLI tool.
+    Config(ConfigArgs),
 }
 
 #[derive(Parser)]
 struct BranchArgs {
-    /// The title of the branch
-    title: String,
+    /// The ticket number
+    ticket_number: String,
 
     /// A description for the branch
     description: String,
+}
+
+#[derive(Parser)]
+struct ConfigArgs {
+    /// Set the parent folder where all Rente projects are contained.
+    #[arg(short = 'p', long)]
+    branch_prefix: Option<String>,
 }
 
 #[derive(Parser)]
@@ -39,31 +54,46 @@ struct CommitArgs {
 fn main() {
     let cli = Cli::parse();
 
-    match &cli.command {
-        Commands::B(args) => {
-            let formatted_args = format!("feature/VERREN-{}-{}", args.title, args.description);
-            let _ = Command::new("git")
-                .args(["checkout", "-b", &formatted_args])
-                .output();
+    match cli.command {
+        Commands::Config(args) => {
+            if args.branch_prefix.is_some() {
+                config::set_config(args.branch_prefix.unwrap())
+            } else {
+                config::display_conifg()
+            }
         }
+
+        Commands::B(args) => { 
+            commands::create_branch(config::get_config().branch_prefix, args.ticket_number, args.description);
+        }
+
         Commands::C(args) => {
-            // let formatted_args = format!("VERREN-{}-{}", args.title, args.description);
-            // let _ = Command::new("git")
-            //     .args(["commit", "-m", &args.message])
-            //     .output();
             let branch = Command::new("git")
                 .args(["symbolic-ref", "--short", "HEAD"])
-                .output();
-            print!("{:?}", branch.unwrap());
-        }
-        Commands::AA => {
+                .output()
+                .expect("Failed to get current git branch name, cannot attempt commit.")
+                .stdout;
+
+            let branch_string = String::from_utf8_lossy(&branch).into_owned();
+            let ticket_number = Regex::new(r"\d+").unwrap().find(&branch_string)
+                .expect("Branch does not contain number, likely does not conform to convention.")
+                .as_str();
+
+            let formatted_args = format!("VERREN-{}:{}", ticket_number, args.message);
             let _ = Command::new("git")
-                .arg("add .")
+                .args(["commit", "-m", &formatted_args])
                 .output();
         }
-        Commands::RA => {
+
+        Commands::A => {
             let _ = Command::new("git")
-                .arg("reset .")
+                .args(["add", "."])
+                .output();
+        }
+
+        Commands::R => {
+            let _ = Command::new("git")
+                .args(["reset", "."])
                 .output();
         }
     }
